8c00645a90b4570ee07d2125860250747e242a8a
Robin Tollisen <al4crity@gmail.com>
2016-04-12 16:02:45 +0200

    Added some additional info to summonerdetails to make it somewhat relevant compared to being the same as shown on live game page.

--- app/components/SummonerDetails.js
+++ app/components/SummonerDetails.js
@@ -48,18 +48,35 @@ export class SummonerDetails extends Component {
             winRate = (divideOrDefault(wins, total, 0)*100).toFixed(0),
             kills = divideOrDefault(getDeepOrDefault(currentChampion, ["stats", "totalChampionKills"], 0), total, 0).toFixed(2),
             assists = divideOrDefault(getDeepOrDefault(currentChampion, ["stats", "totalAssists"], 0), total, 0).toFixed(2),
-            deaths = divideOrDefault(getDeepOrDefault(currentChampion, ["stats", "totalDeathsPerSession"], 0), total, 0).toFixed(2);
+            deaths = divideOrDefault(getDeepOrDefault(currentChampion, ["stats", "totalDeathsPerSession"], 0), total, 0).toFixed(2),
+            minionKills = divideOrDefault(getDeepOrDefault(currentChampion, ["stats", "totalMinionKills"], 0), total, 0).toFixed(1),
+            goldEarned = divideOrDefault(getDeepOrDefault(currentChampion, ["stats", "totalGoldEarned"], 0), total, 0).toFixed(0),
+            championInfo = JSON.stringify(currentChampion);
 
         //console.log("Summoner was defined:");
         //console.log(summoner);
-            return (<View style={styles.container}>
-                <Image
-                    source={{uri: "http://ddragon.leagueoflegends.com/cdn/6.6.1/img/champion/" + this.props.champion.image.full}}
-                    style={styles.thumbnail}
-                />
-                <View style={styles.rightContainer}>
-                    <Text style={styles.name}>{this.props.summonerId}</Text>
-                    <Text style={styles.title}>{league} {division}</Text>
+            return (
+            <View style={styles.outer}>
+                <View style={styles.container}>
+                    <Image
+                        source={{uri: "http://ddragon.leagueoflegends.com/cdn/6.6.1/img/champion/" + this.props.champion.image.full}}
+                        style={styles.thumbnail}
+                    />
+                    <View style={styles.rightContainer}>
+                        <Text style={styles.name}>{this.props.participant.summonerName}</Text>
+                        <Text style={styles.title}>{league} {division}</Text>
+                    </View>
+                    <Image
+                        source={iconsMap[league.toLowerCase()]}
+                        style={styles.thumbnail}
+                    />
+                </View>
+                <View style={styles.containerColumns}>
+                    <Text>W/L: {wins}/{losses} ({winRate}%)</Text>
+                    <Text>K/D/A: {kills}/{assists}/{deaths}</Text>
+                    <Text>Minions: {minionKills}</Text>
+                    <Text>Gold earned: {goldEarned}</Text>
+                    <Text>{championInfo}</Text>
                 </View>
             </View>
         );
@@ -67,19 +84,25 @@ export class SummonerDetails extends Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
+    outer: {
         flex: 1,
+        flexDirection: 'column',
+    },
+    container: {
+        padding: 4,
         flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'flex-start',
-        backgroundColor: '#F5FCFF'
+    },
+    containerColumns: {
+        padding: 4,
+        flexDirection: 'column',
     },
     rightContainer: {
+        paddingTop: 4,
         flex: 1,
     },
     thumbnail: {
-        width: 80,
-        height: 80,
+        width: 60,
+        height: 60,
     },
     name: {
         fontSize: 20,
